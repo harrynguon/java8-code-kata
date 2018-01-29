83fb3fb60d76fba4e2e7969829a329e377844eef
Harry Nguon <harrynguon@gmail.com>
2018-01-29 19:22:18 +1300

    Streams exercise 8 part 1 complete

--- stream-api/src/test/java/stream/api/Exercise8Test.java
+++ stream-api/src/test/java/stream/api/Exercise8Test.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.hasItems;
@@ -27,8 +28,16 @@ public class Exercise8Test extends ClassicOnlineStore {
         /**
          * Create a set of item names that are in {@link Customer.wantToBuy} but not on sale in any shop.
          */
-        List<String> itemListOnSale = null;
-        Set<String> itemSetNotOnSale = null;
+        List<String> itemListOnSale = shopStream.flatMap(shop -> shop.getItemList().stream())
+                .map(Item::getName)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(itemListOnSale);
+        Set<String> itemSetNotOnSale = customerStream.flatMap(c -> c.getWantToBuy().stream())
+                .map(Item::getName)
+                .filter(itemName -> !itemListOnSale.contains(itemName))
+                .collect(Collectors.toSet());
+        System.out.println(itemSetNotOnSale);
 
         assertThat(itemSetNotOnSale, hasSize(3));
         assertThat(itemSetNotOnSale, hasItems("bag", "pants", "coat"));
